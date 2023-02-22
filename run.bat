2a595b6205602cc5c272e045ded40c100f886440
Ed Orsini <edorsini@edsmbp1inch2021.lan>
2023-02-22 15:27:08 -0500

    remove the .bat and .sh files

--- run.bat
+++
@@ -1,2 +0,0 @@
-call mvn clean compile
-call mvn exec:java -D"exec.executable"="edu.bu.met.cs665.Main" -D"log4j.configuration"="file:log4j.properties"
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn clean compile
-mvn exec:java -Dexec.executable="edu.utexas.cs.cs378.Main" -Dexec.args="WikipediaPagesOneDocPerLine.txt.bz2"
